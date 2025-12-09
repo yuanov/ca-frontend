f043b49bcd645530abdb9cc263cf8e605b895eac
Yuanov Yuriy <yura@asigna.io>
2025-12-09 11:00:55 +0400

    init

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import Home from './pages/Home.jsx';
+
+export default function App() {
+  return <Home />;
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CoinInfoChart from '../components/CoinInfoChart.jsx';
+
+export default function Home() {
+  return (
+    <div>
+      <p className="caption">Данные загружаются с локального сервера (localhost:3000)</p>
+      <CoinInfoChart />
+    </div>
+  );
+}
